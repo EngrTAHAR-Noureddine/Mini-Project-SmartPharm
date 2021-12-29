b130a19eca680a739d0c4efdc50d61959b7f6db2
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-12-29 13:54:02 +0100

    add demande order fragment

---
+++ app/src/main/java/com/example/smartpharm/client/demande_order/DemandeOrderFragment.kt
@@ -0,0 +1,32 @@
+package com.example.smartpharm.client.demande_order
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.smartpharm.R
+
+class DemandeOrderFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DemandeOrderFragment()
+    }
+
+    private lateinit var viewModel: DemandeOrderViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.demande_order_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DemandeOrderViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
