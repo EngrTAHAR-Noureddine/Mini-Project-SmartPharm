cc4b41617f1bb3428d2d964fab6dbfce9ee44238
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-12-29 13:51:44 +0100

    add activities : client,login,pharmacist...kt

---
+++ app/src/main/java/com/example/smartpharm/client/clientActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smartpharm.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smartpharm.R
+
+class clientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client)
+    }
+}
