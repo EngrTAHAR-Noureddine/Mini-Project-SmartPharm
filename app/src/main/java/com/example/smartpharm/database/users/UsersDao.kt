83fd526bc3b796ea442426f4255c67ebf19f410a
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-01-07 18:57:29 +0100

    remove suspend

--- app/src/main/java/com/example/smartpharm/database/users/UsersDao.kt
+++ app/src/main/java/com/example/smartpharm/database/users/UsersDao.kt
@@ -1,25 +1,22 @@
 package com.example.smartpharm.database.users
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import com.example.smartpharm.model.User
 
 
 @Dao
 interface UsersDao {
-    @Insert
-    suspend fun insertUser(user: User)
-
-    @Query("DELETE FROM users_table")
-    suspend fun deleteAllUsers()
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUser(user: User)
 
     @Query("SELECT * FROM users_table ORDER BY userId ASC")
     fun getAllUsers(): LiveData<List<User>>
 
+    @Query("DELETE FROM users_table")
+    fun clear()
+
     @Update
-    suspend fun updateUser(user: User)
+    fun updateUser(user: User)
 
 }
