00d31feeb30edc9cd2a9c9d88ed8324a2b985153
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-01-07 18:57:51 +0100

    add login and test it

--- app/src/main/java/com/example/smartpharm/login/main_login/LoginFragment.kt
+++ app/src/main/java/com/example/smartpharm/login/main_login/LoginFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.smartpharm.R
 import com.example.smartpharm.database.users.UsersDataBase
@@ -21,10 +22,23 @@ class LoginFragment : Fragment() {
 
         val application = requireNotNull(this.activity).application
         val dataSource = UsersDataBase.getInstance(application)?.UsersDao()!!
-        val viewModelFactory = LogInViewModelFactory(dataSource)
+        val viewModelFactory = LogInViewModelFactory(dataSource, binding ,this.requireActivity())
         val logInViewModel = ViewModelProvider(this, viewModelFactory)[LoginViewModel::class.java]
         binding.loginViewModel = logInViewModel
         binding.lifecycleOwner = this
+
+        logInViewModel.users.observe(
+            viewLifecycleOwner,
+            {
+                it?.let {
+                    val text = "User couldn't find : users in fragment ${it.size}"
+                    val duration = Toast.LENGTH_SHORT
+                    val toast = Toast.makeText(context, text, duration)
+                    toast.show()
+                }
+            }
+        )
+
 
 
         return binding.root
