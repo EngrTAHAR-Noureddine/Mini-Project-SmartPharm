035e2e261ee3fbc40ad9620f3cffb64f4042bb85
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-01-11 20:56:40 +0100

    set function for button LogOut

--- app/src/main/java/com/example/smartpharm/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/smartpharm/settings/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.example.smartpharm.settings
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smartpharm.R
 import com.example.smartpharm.databinding.SettingsFragmentBinding
 import com.example.smartpharm.firebase.models.User
+import com.example.smartpharm.login.LoginActivity
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 
@@ -21,6 +24,7 @@ class SettingsFragment : Fragment() {
         val prefUser = activity?.getSharedPreferences("UserProfile", Context.MODE_PRIVATE)
         return prefUser?.getString("userProfile","")
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +41,22 @@ class SettingsFragment : Fragment() {
             Picasso.with(context).load(user.photoUser).fit().centerCrop().into(binding.PhotoUser)
             this.binding.recycleViewSetting.layoutManager = LinearLayoutManager(activity)
             this.binding.recycleViewSetting.adapter = SettingRecycleViewAdapter(activity, user)
+        }
+        binding.buttonLogOut.setOnClickListener{
+            val pref = activity?.getSharedPreferences("TypeUserFile", Context.MODE_PRIVATE)
+            val editor : SharedPreferences.Editor? = pref?.edit()
+            editor?.apply{
+                putString("typeUserFile",null)
+            }?.apply()
+            val prefUser = activity?.getSharedPreferences("UserProfile", Context.MODE_PRIVATE)
+            val editorUser : SharedPreferences.Editor? = prefUser?.edit()
+            editorUser?.apply{
+                putString("userProfile","")
+            }?.apply()
+
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
         }
 
         return binding.root
