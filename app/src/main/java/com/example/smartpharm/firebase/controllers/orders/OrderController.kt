9079c742adc6194c0e9a3a978c4399561b686524
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-01-14 18:02:05 +0100

    add id in order and fix note order, add functions : deleteOrder, rejectOrder, AcceptOrder

--- app/src/main/java/com/example/smartpharm/firebase/controllers/orders/OrderController.kt
+++ app/src/main/java/com/example/smartpharm/firebase/controllers/orders/OrderController.kt
@@ -8,22 +8,21 @@ import com.example.smartpharm.firebase.DataBase.db
 import com.example.smartpharm.firebase.models.Order
 import com.example.smartpharm.firebase.models.User
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import java.util.*
 import kotlin.collections.ArrayList
 
 object OrderController {
 
     var listOrders = MutableLiveData<MutableList<Order>?>()
+    var noteOrder  = MutableLiveData<String?>()
+
+    var listState : List<String> = listOf("En cours","Rejeter","Accepter")
 
     lateinit var dataBaseRef : DatabaseReference
 
-    fun createOrder(
-        user: User,
-        pharmacy: User,
-        note: String,
-        files: ArrayList<String>,
-        state: String
-    ): Order {
+    fun createOrder(user: User, pharmacy: User, note: String, files: ArrayList<String>, state: String): Order {
 
         val mapPharmacy: Map<String, String> = mapOf( "namePharmacy" to pharmacy.company,
                                                     "locationPharmacy" to pharmacy.locationUser,
@@ -37,6 +36,7 @@ object OrderController {
                                                 )
 
         return Order(
+            idOrder = UUID.randomUUID().toString(),
             note = note,
             pharmacy = mapPharmacy,
             user = mapUser,
@@ -50,7 +50,7 @@ object OrderController {
 
 
     fun postOrder(order:Order,context:Context){
-        db.collection("Order").document(UUID.randomUUID().toString())
+        db.collection("Order").document(order.idOrder)
             .set(order)
             .addOnSuccessListener { Toast.makeText(context, "Success Upload", Toast.LENGTH_SHORT).show() }
             .addOnFailureListener { Toast.makeText(context, "Failed Upload", Toast.LENGTH_SHORT).show()}
@@ -61,13 +61,13 @@ object OrderController {
             .get()
             .addOnSuccessListener { querySnapshot ->run{
                 if (!querySnapshot.isEmpty) {
-                    var list:MutableList<Order>? = querySnapshot.toObjects(Order::class.java)
+                    var list:List<Order>? = querySnapshot.toObjects(Order::class.java)
                     listOrders.value =if(user!=null) when(user.typeUser){
                                                                             "Client" -> (list?.filter { item  -> item.userEmail == user.emailUser }) as MutableList<Order>?
                                                                              else -> (list?.filter { item  -> item.pharmacyEmail == user.emailUser }) as MutableList<Order>?
                                                                         } else null
                 }else{
-                    val text = "The Pharmacy hasn't the medications"
+                    val text = "The Pharmacy hasn't the order"
                     val duration = Toast.LENGTH_SHORT
                     val toast = Toast.makeText(context, text, duration)
                     toast.show()
@@ -82,6 +82,46 @@ object OrderController {
                 toast.show()
             }
             }
+    }
+
+    fun deleteOrder(order:Order,context: Context){
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val photoList = order.photoOrders
+
+        db.collection("Order").document(order.idOrder)
+            .delete()
+            .addOnSuccessListener {
+               Log.d("FIRESTORE", "DocumentSnapshot successfully deleted!")
+               Toast.makeText(context, "successfully deleted!", Toast.LENGTH_SHORT).show()
+               val list =if(listOrders.value!=null) listOrders.value!!.filter {item:Order-> item.idOrder!= order.idOrder}else null
+               listOrders.value = list as MutableList<Order>?
+
+                if(!photoList.isNullOrEmpty()){
+                    for (item in photoList){
+                        val riversRef = storageRef.child("ImagesSmartPharm/${item}")
+                        riversRef.delete().addOnSuccessListener {
+                            Log.d("FIRESTORE", "DocumentSnapshot successfully deleted!")
+                        }.addOnFailureListener {
+                            Log.d("FIRESTORE", "DocumentSnapshot Failed deleted!")
+                        }
+                    }
+                }
+            }
+            .addOnFailureListener {  Toast.makeText(context, "Delete Failed!", Toast.LENGTH_SHORT).show() }
+    }
+
+    fun changeStateOrder(order:Order,context: Context,range:Int){
+        db.collection("Order").document(order.idOrder)
+            .update("state", listState[range])
+            .addOnSuccessListener {
+                Log.d("FIRESTORE", "DocumentSnapshot successfully deleted!")
+                Toast.makeText(context, "successfully deleted!", Toast.LENGTH_SHORT).show()
+                val list : MutableList<Order>? = listOrders.value
+                list?.find { item -> item.idOrder == order.idOrder}?.state = listState[range]
+                listOrders.value = list
+            }
+            .addOnFailureListener {  Toast.makeText(context, "Delete Failed!", Toast.LENGTH_SHORT).show() }
     }
 
 
