13b7dc250dda330435412758e6cec21a060be66e
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-01-14 22:06:16 +0100

    clean code

--- app/src/main/java/com/example/smartpharm/pharmacist/home/ListPharmacyOrder.kt
+++ app/src/main/java/com/example/smartpharm/pharmacist/home/ListPharmacyOrder.kt
@@ -40,18 +40,4 @@ class PharmacyOrderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val item = view.findViewById<View>(R.id.itemOrderPharmacy) as View
     val testItem = view.findViewById<TextView>(R.id.testItem2) as TextView
     val removeButton = view.findViewById<ImageButton>(R.id.removeButton) as ImageButton
-
-
 }
-
-//TODO Recycle view OF Order for pharmacy
-// TODO Recycle view OF Order for user
-// TODO view Order fragment
-// TODO ADD REMOVE ORDER -> TODO Update Order  when rejet (like cancel )  order from pharmacy remove it from both
-// TODO ADD ACCEPT ORDER -> TODO Update Order
-// TODO POPMenu of Ville
-// TODO SEARCH BARs
-// MAP AFTER
-//UI Design
-
-// */
