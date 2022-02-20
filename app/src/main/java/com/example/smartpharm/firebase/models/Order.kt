2908b8bb5cddb9fd78eede2de1b4d4248092786f
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-02-20 11:52:53 +0100

    [remove] unnecessary

--- app/src/main/java/com/example/smartpharm/firebase/models/Order.kt
+++ app/src/main/java/com/example/smartpharm/firebase/models/Order.kt
@@ -2,17 +2,6 @@ package com.example.smartpharm.firebase.models
 
 import com.google.firebase.database.IgnoreExtraProperties
 import com.google.gson.annotations.SerializedName
-/*
-data class Order (
-    @SerializedName("emailUser") var nameUser: String = "",
-    @SerializedName("locationUser") var locationUser: String = "",
-    @SerializedName("name") var phoneUser: String = "",
-    @SerializedName("passwordUser") var emailUser: String = "",
-    @SerializedName("phoneNumber") var phoneNumber: String = "",
-    @SerializedName("photoUser") var photoUser: String = "",
-    @SerializedName("typeUser") var typeUser: String = "",
-    @SerializedName("company") var company: String = ""
-)*/
 @IgnoreExtraProperties
 data class Order (
     @SerializedName("idOrder") var idOrder: String = "",
