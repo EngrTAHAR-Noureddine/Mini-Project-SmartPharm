bcd56dc6a27b58873969d6ea8debd948c94a994e
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-02-20 11:51:27 +0100

    [add] navigation controller to go to the order details for pharmacy and user

--- app/src/main/java/com/example/smartpharm/pharmacist/home/ListPharmacyOrder.kt
+++ app/src/main/java/com/example/smartpharm/pharmacist/home/ListPharmacyOrder.kt
@@ -1,5 +1,8 @@
 package com.example.smartpharm.pharmacist.home
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,11 +10,13 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.smartpharm.R
-import com.example.smartpharm.firebase.controllers.orders.OrderController.changeStateOrder
-import com.example.smartpharm.firebase.controllers.orders.OrderController.deleteOrder
+import com.example.smartpharm.firebase.controllers.orders.OrderController
+import com.example.smartpharm.firebase.controllers.orders.OrderController.listState
 import com.example.smartpharm.firebase.models.Order
+import com.google.gson.Gson
 
 class ListPharmacyOrder(val context: FragmentActivity?, var data:List<Order>?):
     RecyclerView.Adapter<PharmacyOrderViewHolder>() {
@@ -19,16 +24,43 @@ class ListPharmacyOrder(val context: FragmentActivity?, var data:List<Order>?):
         return PharmacyOrderViewHolder(LayoutInflater.from(context).inflate(R.layout.pharmacy_order_item, parent, false))
     }
 
+    @SuppressLint("CommitPrefEdits")
     override fun onBindViewHolder(holder: PharmacyOrderViewHolder, position: Int) {
         holder.testItem.text =if(data!=null && data!!.isNotEmpty()) data!![position].state+"\n"+ data!![position].idOrder else ""
         holder.removeButton.isVisible = false
         holder.item.setOnClickListener {
-            holder.removeButton.isVisible = !holder.removeButton.isVisible
-        }
-        holder.removeButton.setOnClickListener {
-            if(!data.isNullOrEmpty()) changeStateOrder(data!![position],context!!,2)
+
+            if(data!=null && data!!.isNotEmpty()){
+                val order:Order? = data!![position]
+                val gson = Gson()
+                val prefOrder = context?.getSharedPreferences("Order", Context.MODE_PRIVATE)
+                val editorOrder : SharedPreferences.Editor? = prefOrder?.edit()
+                val json = gson.toJson(order)
+                editorOrder?.apply{
+                    putString("orderDetail",json)
+                }?.apply()
+
+                val userType: String = getData("TypeUserFile", "typeUserFile") ?: ""
+                if(data!![position].state == listState[2]){
+                    holder.removeButton.isVisible = !holder.removeButton.isVisible
+                }else{
+                    if(userType == "Pharmacy"){
+                        context?.findNavController(R.id.myPharmacyNavHostFragment)?.navigate(R.id.action_destination_to_Pharmacy_Order_Detail)
+                    }else{
+                        context?.findNavController(R.id.myNavHostFragment)?.navigate(R.id.action_destination_toOrder_Detail)
+                    }
+                }
+            }
         }
 
+        holder.removeButton.setOnClickListener {
+           if(data!=null) OrderController.deleteOrder(data!![position], context!!)
+        }
+
+    }
+    private fun getData(file:String, string: String): String?{
+        val prefUser = context?.getSharedPreferences(file, Context.MODE_PRIVATE)
+        return prefUser?.getString(string,"")
     }
 
     override fun getItemCount() = if(data != null && data!!.isNotEmpty()) data!!.size else 0
@@ -37,7 +69,7 @@ class ListPharmacyOrder(val context: FragmentActivity?, var data:List<Order>?):
 }
 
 class PharmacyOrderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val item = view.findViewById<View>(R.id.itemOrderPharmacy) as View
-    val testItem = view.findViewById<TextView>(R.id.testItem2) as TextView
-    val removeButton = view.findViewById<ImageButton>(R.id.removeButton) as ImageButton
+    val item = view.findViewById(R.id.itemOrderPharmacy) as View
+    val testItem = view.findViewById(R.id.testItem2) as TextView
+    val removeButton = view.findViewById(R.id.removeButton) as ImageButton
 }
