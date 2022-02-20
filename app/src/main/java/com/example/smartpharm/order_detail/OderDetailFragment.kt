6473e6c8576bd5bdfaa837a27fda8d736d4549d5
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-02-20 11:55:20 +0100

    [add] code to show data of order with adding accept and reject button functions

--- app/src/main/java/com/example/smartpharm/order_detail/OderDetailFragment.kt
+++ app/src/main/java/com/example/smartpharm/order_detail/OderDetailFragment.kt
@@ -1,34 +1,119 @@
 package com.example.smartpharm.order_detail
 
-import androidx.lifecycle.ViewModelProvider
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.smartpharm.R
+import com.example.smartpharm.client.demande_order.GridImageAdapter
+import com.example.smartpharm.controllers.FileController
+import com.example.smartpharm.controllers.FileController.destroyAllFiles
+import com.example.smartpharm.controllers.FileController.listFile
+import com.example.smartpharm.controllers.FileController.returnPhotos
+import com.example.smartpharm.databinding.OderDetailFragmentBinding
+import com.example.smartpharm.firebase.controllers.orders.OrderController.listState
+import com.example.smartpharm.firebase.models.Order
+import com.example.smartpharm.firebase.models.User
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+
 
 class OderDetailFragment : Fragment() {
 
-    // detail sur Oder : afficher photo , nome client ,address cleint ..., ( les info de client )
-
-    companion object {
-        fun newInstance() = OderDetailFragment()
-    }
 
     private lateinit var viewModel: OderDetailViewModel
+    private lateinit var mainBinding: OderDetailFragmentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.oder_detail_fragment, container, false)
+    private fun getData(file:String, string: String): String?{
+        val prefUser = context?.getSharedPreferences(file, Context.MODE_PRIVATE)
+        return prefUser?.getString(string,"")
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(OderDetailViewModel::class.java)
-        // TODO: Use the ViewModel
+    @SuppressLint("SetTextI18n")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        mainBinding = DataBindingUtil.inflate(inflater,R.layout.oder_detail_fragment,container,false)
+
+        val gson = Gson()
+        val json1: String = getData("Order", "orderDetail") ?: ""
+        val json2: String = getData("UserProfile", "userProfile") ?: ""
+        val order :Order? = gson.fromJson(json1, Order::class.java)
+        val user : User? = gson.fromJson(json2, User::class.java)
+        var type = ""
+
+        if(user !=null) type = user.typeUser
+
+
+        val viewModelFactory = OrderDetailFactory(mainBinding ,this.requireActivity(), type, order)
+
+        val orderDetailViewModel = ViewModelProvider(this, viewModelFactory)[OderDetailViewModel::class.java]
+
+        this.mainBinding.orderDetailViewModel = orderDetailViewModel
+        this.mainBinding.lifecycleOwner = this
+
+        mainBinding.GridRecycleView.layoutManager = GridLayoutManager(context, 3)
+        //-------------------------------
+        if(order!=null && user !=null) {
+            if (user.typeUser == "Pharmacy") {
+                Picasso.with(context).load(order.user!!["photoUser"]).fit().centerCrop()
+                    .into(mainBinding.imageOrder)
+
+                mainBinding.nameDetailPharmacy.text = order.user!!["nameUser"]
+                mainBinding.locationDetailPharmacy.text = order.user!!["locationUser"]
+
+                if (order.state == listState[0]) {
+                    mainBinding.buttonRejectOrder.isVisible = true
+                    mainBinding.buttonAcceptOrder.isVisible = true
+                } else {
+                    mainBinding.buttonRejectOrder.isVisible = false
+                    mainBinding.buttonAcceptOrder.isVisible = false
+                }
+            } else {
+                if (order.state == listState[0]) {
+                    mainBinding.buttonRejectOrder.isVisible = false
+                    mainBinding.buttonAcceptOrder.text = "Annuler"
+                    mainBinding.buttonAcceptOrder.isVisible = true
+                } else {
+                    mainBinding.buttonRejectOrder.isVisible = false
+                    mainBinding.buttonAcceptOrder.isVisible = false
+                }
+                Log.v("PHOTOTAG","photo ${order.pharmacy!!["photoPharmacy"]}")
+                Picasso.with(context).load(order.pharmacy!!["photoPharmacy"]).fit().centerCrop()
+                    .into(mainBinding.imageOrder)
+                mainBinding.nameDetailPharmacy.text = order.pharmacy!!["namePharmacy"]
+                mainBinding.locationDetailPharmacy.text = order.pharmacy!!["locationPharmacy"]
+            }
+
+            returnPhotos(order.photoOrders!!)
+            listFile.observe(
+                viewLifecycleOwner
+            ) {
+                mainBinding.GridRecycleView.adapter = GridImageAdapter(activity, it)
+            }
+            mainBinding.inputNote.setText(order.note)
+        }
+        //-------------------------------
+
+        val navBar = activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        if(navBar != null){
+            navBar.isVisible = false
+        }
+        return mainBinding.root
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        destroyAllFiles()
+    }
+
 
 }
--- app/src/main/java/com/example/smartpharm/order_detail/OderDetailViewModel.kt
+++ app/src/main/java/com/example/smartpharm/order_detail/OderDetailViewModel.kt
@@ -1,7 +1,80 @@
 package com.example.smartpharm.order_detail
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
+import com.example.smartpharm.R
+import com.example.smartpharm.databinding.OderDetailFragmentBinding
+import com.example.smartpharm.firebase.controllers.orders.OrderController.changeStateOrder
+import com.example.smartpharm.firebase.controllers.orders.OrderController.deleteOrder
+import com.example.smartpharm.firebase.models.Order
 
-class OderDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class OderDetailViewModel(private val binding: OderDetailFragmentBinding,
+                          private val context: FragmentActivity,
+                          private val type : String,
+                          private val order : Order? ) : ViewModel() {
+
+
+    fun phoneNumber(){
+        if(order != null){
+            val tel = if(type == "Pharmacy") order.user!!["phoneUser"] else order.pharmacy!!["phonePharmacy"]
+            val url = Uri.parse("tel:$tel")
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url.toString()))
+            try {
+                context.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    context,
+                    "You haven't phone application to make a call",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+    fun locationUser(){
+        if(order != null){
+            val latitude = 28.0339
+            val longitude = 1.6596
+            val url = Uri.parse("geo:$latitude,$longitude")
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url.toString()))
+            try {
+                context.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    context,
+                    "You haven't an application to find a location",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    fun acceptOrder(){
+       if(order!=null) if(type == "Pharmacy"){
+           Log.v("TAG BUTTON", "ACCEPTED PHARMACY")
+           changeStateOrder(order,context,2)
+           context.findNavController(R.id.myPharmacyNavHostFragment).popBackStack()
+       } else{
+           deleteOrder(order,context)
+           Log.v("TAG BUTTON", "REJECTED")
+           context.findNavController(R.id.myNavHostFragment).popBackStack()
+       }
+    }
+    fun rejectOrder(){
+        if(order!=null){
+            deleteOrder(order,context)
+            if(type == "Pharmacy") {
+                Log.v("TAG BUTTON", "REJECTED PHARMACY")
+                context.findNavController(R.id.myPharmacyNavHostFragment).popBackStack()
+            }else{
+                Log.v("TAG BUTTON", "REJECT USER")
+                context.findNavController(R.id.myNavHostFragment).popBackStack()
+            }
+        }
+    }
 }
