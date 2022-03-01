3faf2d91aff9e5dab6d1378aeaa75f8fe452d6c3
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-03-01 21:27:27 +0100

    [add] save data , insert , update, clear table in database

--- app/src/main/java/com/example/smartpharm/controllers/MedicationController.kt
+++ app/src/main/java/com/example/smartpharm/controllers/MedicationController.kt
@@ -1,15 +1,16 @@
 package com.example.smartpharm.controllers
 
+import android.app.Application
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.smartpharm.database.DataBase
-import com.example.smartpharm.models.Medication
-import com.example.smartpharm.models.MyMedications
-import com.example.smartpharm.models.Order
-import com.example.smartpharm.models.User
+import com.example.smartpharm.database.MedicationDao
+import com.example.smartpharm.database.OffLineDB
+import com.example.smartpharm.models.*
 import java.util.*
 
 object MedicationController {
@@ -17,14 +18,65 @@ object MedicationController {
     var listMedications = MutableLiveData<MutableList<Medication>?>()
     var listMedicationsUser = MutableLiveData<MutableList<MyMedications>?>()
 
+    private lateinit var medicationDao: MedicationDao
+
+    private fun init(application: Application){
+        medicationDao = OffLineDB.getInstance(application)?.MedicationDao()!!
+    }
+
+    private fun updateFBFromDB(application: Application){
+        init(application)
+       var medications : LiveData<List<MedicationDB>> = medicationDao.getAllMedications()
+
+
+        if(! medicationDao.getAllMedications().value.isNullOrEmpty()){
+            for (medDB in medications.value!!){
+                var med = ConverterMedicationClasses.toMyMedication(medDB)
+                if(med.Days <0){
+                    DataBase.db.collection("My_Medications").document(med.idMedication)
+                        .delete()
+                        .addOnSuccessListener {
+                            Log.d("MED", "DocumentSnapshot successfully!")
+                        }
+                        .addOnFailureListener {  Log.d("MED", "DocumentSnapshot Failed!") }
+                }else{
+                    DataBase.db.collection("My_Medications").document(med.idMedication)
+                        .update("Dinner", med.Dinner , "Launch",med.Launch,"Days",med.Days)
+                        .addOnSuccessListener {
+                            Log.d("MED", "DocumentSnapshot successfully!")
+                        }
+                        .addOnFailureListener {  Log.d("MED", "DocumentSnapshot Failed!") }
+                }
+            }
+        }
+    }
+
+    private fun insertAllMedications(listMedication:List<MyMedications>){
+        if(!listMedication.isNullOrEmpty()){
+            medicationDao.clear()
+            for (med in listMedication){
+                var medDB = ConverterMedicationClasses.toMedicationDB(med)
+                medicationDao.insert(medDB)
+            }
+        }
+    }
+
     fun getMedicationOfClient(user: User?, context:FragmentActivity){
+
+        updateFBFromDB(context.application)
+
         DataBase.db.collection("My_Medications")
             .get()
             .addOnSuccessListener { querySnapshot ->run{
+
+
                 if (!querySnapshot.isEmpty) {
+
                     var list:MutableList<MyMedications>? = querySnapshot.toObjects(MyMedications::class.java)
                     listMedicationsUser.value = if(user!=null) list?.filter{ item -> (item.idUser == user.idUser)} as MutableList<MyMedications>? else null
+                    listMedicationsUser.value?.let { insertAllMedications(it) }
                 }else{
+                    medicationDao.clear()
                     val text = "The Client hasn't the medications"
                     val duration = Toast.LENGTH_SHORT
                     val toast = Toast.makeText(context, text, duration)
@@ -33,6 +85,16 @@ object MedicationController {
             }
             }
             .addOnFailureListener { exception ->run{
+                 val listMedDB = medicationDao.getAllMedications().value as MutableList<MedicationDB>?
+                if(!listMedDB.isNullOrEmpty()){
+                    var list = mutableListOf<MyMedications>()
+                    listMedDB.forEach {
+                        list.add(ConverterMedicationClasses.toMyMedication(it))
+                    }
+                    listMedicationsUser.value = list
+                }else{
+                    listMedicationsUser.value = null
+                }
                 Log.w("FIRESTORE", "Error getting documents $exception")
                 val text = "Out Connexion"
                 val duration = Toast.LENGTH_SHORT
@@ -53,6 +115,9 @@ object MedicationController {
             }
             .addOnFailureListener {  Log.d("MED", "DocumentSnapshot Failed!") }
     }
+
+
+
 
     fun getMedicationOf(pharmacy: User?, context:FragmentActivity){
         DataBase.db.collection("Medication")
