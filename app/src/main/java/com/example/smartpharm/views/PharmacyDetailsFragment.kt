1fba8d8fa0ae83bc9b744227b830df8ebba328b8
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-03-05 17:42:52 +0100

    [add] functions to go to the profile of user

--- app/src/main/java/com/example/smartpharm/views/PharmacyDetailsFragment.kt
+++ app/src/main/java/com/example/smartpharm/views/PharmacyDetailsFragment.kt
@@ -1,12 +1,16 @@
 package com.example.smartpharm.views
 
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.smartpharm.databinding.PharmacyDetailsFragmentBinding
 import com.example.smartpharm.models.User
 import com.google.gson.Gson
@@ -47,7 +51,41 @@ class PharmacyDetailsFragment : Fragment() {
             binding.FacebookAccountPharmacy.text = user.facebookAccount
             binding.InstagramPharmacy.text = user.instagramAccount
             binding.PhonePharmacy.text =user.phoneNumber
+
+            binding.FacebookAccountPharmacy.setOnClickListener{
+                val url  = "fb://page/${user.facebookAccount}"
+                try {
+                    openPage(requireContext(), url)
+                }catch (e: ActivityNotFoundException){
+                    val newUrl = url.replace("fb://","http:www.facebook.com/")
+                    openPage(requireContext(), newUrl)
+                }
+            }
+            binding.InstagramPharmacy.setOnClickListener{
+                val url  = "http:www.instagram.com/${user.instagramAccount}"
+                openPage(requireContext(), url)
+            }
+            binding.PhonePharmacy.setOnClickListener {
+
+                    val tel = user.phoneNumber
+                    val url = "tel:$tel"
+                    try {
+                        openPage(requireContext(), url)
+                    } catch (e: ActivityNotFoundException) {
+                        Toast.makeText(
+                            context,
+                            "You haven't phone application to make a call",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+            }
         }
+    }
+
+    private fun openPage(ctx: Context, url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        ctx.startActivity(intent)
     }
 
 
