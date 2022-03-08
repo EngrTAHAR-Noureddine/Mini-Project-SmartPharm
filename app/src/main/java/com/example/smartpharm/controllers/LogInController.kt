a9d39b4c5faf7162250dcad044fd1963566e8eb4
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-03-08 06:46:01 +0100

    [add] login with phone

--- app/src/main/java/com/example/smartpharm/controllers/LogInController.kt
+++ app/src/main/java/com/example/smartpharm/controllers/LogInController.kt
@@ -25,12 +25,11 @@ object LogInController {
 
         var privateUser: User?
 
-        DataBase.db.collection("User")
+        DataBase.db.collection("User").whereEqualTo("emailUser",email).whereEqualTo("passwordUser",password)
             .get()
             .addOnSuccessListener { querySnapshot ->run{
                 if (!querySnapshot.isEmpty) {
                     var list:List<User>? = querySnapshot.toObjects(User::class.java)
-                    list = list?.filter{ item -> (item.emailUser == email && item.passwordUser== password)}
                     privateUser = if(!list.isNullOrEmpty()) list[0] else null
 
                     if(privateUser != null){
@@ -90,10 +89,88 @@ object LogInController {
                         toast.show()
                     }
 
+                }else{
+                    DataBase.db.collection("User").whereEqualTo("phoneNumber",email).whereEqualTo("passwordUser",password)
+                        .get()
+                        .addOnSuccessListener { querySnapshot ->run{
+                            if (!querySnapshot.isEmpty) {
+                                var list:List<User>? = querySnapshot.toObjects(User::class.java)
+                                privateUser = if(!list.isNullOrEmpty()) list[0] else null
+
+                                if(privateUser != null){
+
+                                    val pref = context.getSharedPreferences("TypeUserFile", Context.MODE_PRIVATE)
+                                    val editor : SharedPreferences.Editor = pref.edit()
+                                    editor.apply{
+                                        putString("typeUserFile",privateUser!!.typeUser)
+                                    }.apply()
+
+                                    val gson = Gson()
+                                    val prefUser = context.getSharedPreferences("UserProfile", Context.MODE_PRIVATE)
+                                    val editorUser : SharedPreferences.Editor = prefUser.edit()
+                                    val json = gson.toJson(privateUser)
+                                    editorUser.apply{
+                                        putString("userProfile",json)
+                                    }.apply()
+
+                                    //FCM
+                                    FirebaseMessaging.getInstance().token.addOnCompleteListener(
+                                        OnCompleteListener { task ->
+                                            if (!task.isSuccessful) {
+                                                Log.v("NOTIFICATION TAG", "Fetching FCM registration token failed ${task.exception}")
+                                                return@OnCompleteListener
+                                            }
+
+                                            // Get new FCM registration token
+                                            val token = task.result
+                                            FirebaseMessaging.getInstance().subscribeToTopic(privateUser!!.idUser)
+                                            val pref = context.getSharedPreferences("TokenFCM", Context.MODE_PRIVATE)
+                                            val editorToken : SharedPreferences.Editor = pref.edit()
+                                            Log.v("NOTIFICATION TAG", "Fetching FCM registration token Success ${task.result}")
+                                            editorToken.apply{
+                                                putString("token",token)
+                                            }.apply()
+                                        })
+
+                                    if(privateUser!!.typeUser == "Pharmacy"){
+                                        val intent = Intent(context, PharmacistActivity::class.java)
+                                        context.startActivity(intent)
+                                        context.finish()
+                                    }
+                                    else{ //Client
+                                        val intent = Intent(context, ClientActivity::class.java)
+                                        context.startActivity(intent)
+                                        context.finish()
+                                    }
+
+                                }else{
+                                    val progressBar = context.findViewById<ProgressBar>(R.id.progressBarLogin)
+                                    progressBar.isVisible = false
+                                    val buttonLogin = context.findViewById<Button>(R.id.buttonLogIn)
+                                    buttonLogin.isVisible = true
+                                    val text = "email or password is incorrect"
+                                    val duration = Toast.LENGTH_SHORT
+                                    val toast = Toast.makeText(context, text, duration)
+                                    toast.show()
+                                }
+
+                            }
+                        }
+                        }.addOnFailureListener { exception -> run{
+                            val progressBar = context.findViewById<ProgressBar>(R.id.progressBarLogin)
+                            progressBar.isVisible = false
+                            val buttonLogin = context.findViewById<Button>(R.id.buttonLogIn)
+                            buttonLogin.isVisible = true
+                            Log.w("FIRESTORE", "Error getting documents $exception")
+                            val text = "Out Connexion"
+                            val duration = Toast.LENGTH_SHORT
+                            val toast = Toast.makeText(context, text, duration)
+                            toast.show()
+                        } }
                 }
             }
             }
-            .addOnFailureListener { exception ->run{
+            .addOnFailureListener { exception -> run{
                 val progressBar = context.findViewById<ProgressBar>(R.id.progressBarLogin)
                 progressBar.isVisible = false
                 val buttonLogin = context.findViewById<Button>(R.id.buttonLogIn)
@@ -103,8 +180,6 @@ object LogInController {
                 val duration = Toast.LENGTH_SHORT
                 val toast = Toast.makeText(context, text, duration)
                 toast.show()
-            }
-
-            }
+            }}
     }
 }
